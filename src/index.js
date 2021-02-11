5f138c315a7afefdd37c9eb5c05200a029e7bdae
patrick-rush <patrick.a.rush@gmail.com>
2021-02-11 13:51:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
+  // <ClickityClick />, document.getElementById('root')
   <div>
     Mount Components Here
   </div>,
